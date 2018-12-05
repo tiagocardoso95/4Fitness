ed2eebc3ad2a16728a7a5342d27acf1bb57e9e3a
MunchMunchi <43782986+MunchMunchi@users.noreply.github.com>
2018-12-05 20:25:04 +0000

    Hidration begin and user stats

---
+++ CMProject/app/src/main/java/com/example/tcard/cmproject/UserStats/UserStats.java
@@ -0,0 +1,36 @@
+package com.example.tcard.cmproject.UserStats;
+
+public class UserStats {
+
+    private float weight;
+    private float height;
+
+    private String name;
+
+
+    
+
+    public  float getWeight(){
+        return weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
